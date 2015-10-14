6b98d5d027208aa077e12541e2a90df4df34626f
nikila-saravanan <nikila.saravanan@gmail.com>
2015-10-14 11:26:45 -0400

    done

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,16 @@
 # Implement your procedural solution here!
+def find_fib_seq(num)
+  sequence = [1,2]
+  i = 0
+  while (sequence[-1] + sequence[-2]) < num
+    sequence << (sequence[-1] + sequence[-2])
+  end
+  sequence
+end
+
+def even_fibonacci_sum(num)
+  seq = find_fib_seq(num)
+  evens = seq.select {|x| x%2 == 0}
+  sum = 0
+  evens.inject {|sum,x| sum += x}
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,28 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+
+  def initialize(num)
+    @num = num
+  end
+
+  def find_fib_seq(num)
+    sequence = [1,2]
+    i = 0
+    while (sequence[-1] + sequence[-2]) < num
+      sequence << (sequence[-1] + sequence[-2])
+    end
+    sequence
+  end
+
+  def find_even_fib(num)
+    seq = find_fib_seq(num)
+    even_seq = seq.select {|x| x%2 == 0}
+  end
+
+  def sum
+    evens = find_even_fib(@num)
+    sum = 0
+    evens.inject {|sum, x| sum += x}
+  end
+
+end
